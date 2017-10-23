8f9b8c02c122a518b22c7fe2e0461393ca8b7ac0
SHEN_XIAOCHENG <845876310@qq.com>
2017-10-22 23:56:26 -0500

    add basic login with google

--- app/src/main/java/com/ee382v/sparrow/ee382v_sparrow_mini_phase3/MainActivity.java
+++ app/src/main/java/com/ee382v/sparrow/ee382v_sparrow_mini_phase3/MainActivity.java
@@ -1,13 +1,116 @@
 package com.ee382v.sparrow.ee382v_sparrow_mini_phase3;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener{
+
+    private Button SignOut;
+    private SignInButton SignIn;
+    private TextView Name;
+    private GoogleApiClient googleApiClient;
+    private static final int REQ_CODE = 9001;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SignOut = (Button)findViewById(R.id.bn_logout);
+        SignIn = (SignInButton)findViewById(R.id.bn_login);
+        Name = (TextView)findViewById(R.id.hello);
+        SignIn.setOnClickListener(this);
+        SignOut.setOnClickListener(this);
+        Name.setVisibility(View.GONE);
+        SignOut.setVisibility(View.GONE);
+        GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        googleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this,this).addApi(Auth.GOOGLE_SIGN_IN_API,signInOptions).build();
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.bn_login:
+                signIn();
+                break;
+
+            case R.id.bn_logout:
+                signOut();
+                break;
+        }
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    private void signIn(){
+        Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+        startActivityForResult(intent,REQ_CODE);
+
+    }
+    private void signOut(){
+        Auth.GoogleSignInApi.signOut(googleApiClient).setResultCallback(new ResultCallback<Status>() {
+            @Override
+            public void onResult(@NonNull Status status) {
+                updateUI(false);
+
+            }
+        });
+
+    }
+    private void handleResult(GoogleSignInResult result){
+        if(result.isSuccess()){
+            GoogleSignInAccount account = result.getSignInAccount();
+            String name = account.getDisplayName();
+            String email = account.getEmail();
+            updateUI(true);
+        }
+        else{
+            updateUI(false);
+        }
+
+    }
+    private void updateUI(boolean isLogin){
+        if(isLogin){
+            Name.setVisibility(View.VISIBLE);
+            SignIn.setVisibility(View.GONE);
+            SignOut.setVisibility(View.VISIBLE);
+        }
+        else{
+            Name.setVisibility(View.GONE);
+            SignIn.setVisibility(View.VISIBLE);
+            SignOut.setVisibility(View.GONE);
+
+        }
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode,resultCode,data);
+
+        if(requestCode == REQ_CODE){
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleResult(result);
+        }
+
     }
 }
