de7b093c4728b5935c7e431cb2e16d016c6e5ddd
utpatrick <lordbruce727@gmail.com>
2017-10-25 10:05:51 -0500

    adding camera function

---
+++ app/src/main/java/com/ee382v/sparrow/ee382v_sparrow_mini_phase3/CamActivity.java
@@ -0,0 +1,238 @@
+package com.ee382v.sparrow.ee382v_sparrow_mini_phase3;
+
+/**
+ * @author Jose Davis Nidhin
+ */
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.hardware.Camera;
+import android.hardware.Camera.PictureCallback;
+import android.hardware.Camera.ShutterCallback;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.View.OnLongClickListener;
+import android.view.ViewGroup.LayoutParams;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class CamActivity extends AppCompatActivity {
+    private static final String TAG = "CamActivity";
+    CamPreview preview;
+    Button buttonClick;
+    Camera camera;
+    Activity act;
+    Context ctx;
+    String fileName;
+    File sdCard = Environment.getExternalStorageDirectory();
+    File dir = new File (sdCard.getAbsolutePath() + "/camtest");
+    private ImageView capturedImageHolder;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ctx = this;
+        act = this;
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_camera);
+
+        preview = new CamPreview(this, (SurfaceView)findViewById(R.id.surfaceView));
+        preview.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+        ((FrameLayout) findViewById(R.id.layout)).addView(preview);
+        preview.setKeepScreenOn(true);
+        capturedImageHolder = (ImageView)findViewById(R.id.captured_image);
+
+        preview.setOnLongClickListener(new OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                camera.autoFocus(new Camera.AutoFocusCallback() {
+                    @Override
+                    public void onAutoFocus(boolean arg0, Camera arg1) {
+                        //camera.takePicture(shutterCallback, rawCallback, jpegCallback);
+                    }
+                });
+                return true;
+            }
+        });
+
+        Button captureButton = (Button)findViewById(R.id.button);
+        captureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Camera.Parameters params= camera.getParameters();
+                params.set("rotation", 90);
+                camera.setParameters(params);
+                camera.takePicture(shutterCallback, rawCallback, jpegCallback);
+            }
+        });
+
+        Toast.makeText(ctx, getString(R.string.take_photo_help), Toast.LENGTH_LONG).show();
+
+        //		buttonClick = (Button) findViewById(R.id.btnCapture);
+        //
+        //		buttonClick.setOnClickListener(new OnClickListener() {
+        //			public void onClick(View v) {
+        ////				preview.camera.takePicture(shutterCallback, rawCallback, jpegCallback);
+        //				camera.takePicture(shutterCallback, rawCallback, jpegCallback);
+        //			}
+        //		});
+        //
+        //		buttonClick.setOnLongClickListener(new OnLongClickListener(){
+        //			@Override
+        //			public boolean onLongClick(View arg0) {
+        //				camera.autoFocus(new AutoFocusCallback(){
+        //					@Override
+        //					public void onAutoFocus(boolean arg0, Camera arg1) {
+        //						//camera.takePicture(shutterCallback, rawCallback, jpegCallback);
+        //					}
+        //				});
+        //				return true;
+        //			}
+        //		});
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        int numCams = Camera.getNumberOfCameras();
+        if(numCams > 0){
+            try{
+                camera = Camera.open(0);
+                camera.startPreview();
+                preview.setCamera(camera);
+            } catch (RuntimeException ex){
+                Toast.makeText(ctx, getString(R.string.camera_not_found), Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        if(camera != null) {
+            camera.stopPreview();
+            preview.setCamera(null);
+            camera.release();
+            camera = null;
+        }
+        super.onPause();
+    }
+
+    private void resetCam() {
+        camera.startPreview();
+        preview.setCamera(camera);
+    }
+
+    private void refreshGallery(File file) {
+        Intent mediaScanIntent = new Intent( Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        mediaScanIntent.setData(Uri.fromFile(file));
+        sendBroadcast(mediaScanIntent);
+    }
+
+    ShutterCallback shutterCallback = new ShutterCallback() {
+        public void onShutter() {
+            //			 Log.d(TAG, "onShutter'd");
+        }
+    };
+
+    PictureCallback rawCallback = new PictureCallback() {
+        public void onPictureTaken(byte[] data, Camera camera) {
+            //			 Log.d(TAG, "onPictureTaken - raw");
+        }
+    };
+
+    PictureCallback jpegCallback = new PictureCallback() {
+        public void onPictureTaken(byte[] data, Camera camera) {
+            Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+            new SaveImageTask().execute(data);
+            resetCam();
+            Log.d(TAG, "onPictureTaken - jpeg");
+            capturedImageHolder.setImageBitmap(scaleDownBitmapImage(bitmap, 300, 200));
+        }
+    };
+
+    private Bitmap scaleDownBitmapImage(Bitmap bitmap, int newWidth, int newHeight){
+        Bitmap resizedBitmap = Bitmap.createScaledBitmap(bitmap, newWidth, newHeight, true);
+        return resizedBitmap;
+    }
+
+    public static Bitmap rotate(Bitmap bitmap, int degree) {
+        int w = bitmap.getWidth();
+        int h = bitmap.getHeight();
+
+        Matrix mtx = new Matrix();
+        mtx.postRotate(degree);
+
+        return Bitmap.createBitmap(bitmap, 0, 0, w, h, mtx, true);
+    }
+
+    private class SaveImageTask extends AsyncTask<byte[], Void, Void> {
+
+        @Override
+        protected Void doInBackground(byte[]... data) {
+            FileOutputStream outStream = null;
+
+            // Write to SD Card
+            try {
+                dir.mkdirs();
+
+                fileName = String.format("%d.jpg", System.currentTimeMillis());
+                File outFile = new File(dir, fileName);
+
+                outStream = new FileOutputStream(outFile);
+                outStream.write(data[0]);
+                outStream.flush();
+                outStream.close();
+
+                Log.d(TAG, "onPictureTaken - wrote bytes: " + data.length + " to " + outFile.getAbsolutePath());
+
+                refreshGallery(outFile);
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+            }
+            return null;
+        }
+
+    }
+
+    public void useThisPhoto(View view) {
+        Intent returnIntent = new Intent();
+        Log.d(TAG, "gg dir from here" + Uri.fromFile(dir).toString());
+        Log.d(TAG, "gg filename from here" + Uri.fromFile(new File(fileName)).toString());
+        Log.d(TAG, "gg uri from here" + Uri.fromFile(new File(dir, fileName)).toString());
+        returnIntent.setData(Uri.fromFile(new File(dir, fileName)));
+        setResult(Activity.RESULT_OK,returnIntent);
+        finish();
+    }
+
+    public void goToView(View view) {
+        Intent intent = new Intent(this, ViewAllStream.class);
+        startActivity(intent);
+    }
+}
+
+
--- app/src/main/java/com/ee382v/sparrow/ee382v_sparrow_mini_phase3/MainActivity.java
+++ app/src/main/java/com/ee382v/sparrow/ee382v_sparrow_mini_phase3/MainActivity.java
@@ -1,6 +1,10 @@
 package com.ee382v.sparrow.ee382v_sparrow_mini_phase3;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,16 +14,26 @@ public class MainActivity extends AppCompatActivity {
     // 10.0.2.2 is the localhost on dev machine
     private static final String BACKEND_ENDPOINT = "http://10.0.2.2:8080";
     public static final String EXTRA_MESSAGE = "com.ees82v.sparrow.mainactivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE }, 0);
+        }
     }
 
     public void goToView(View view) {
-        Intent intent = new Intent(this, ViewAllStream.class);
+        Intent intent = new Intent(MainActivity.this, ViewAllStream.class);
         EditText nameField = (EditText) findViewById(R.id.mainName);
         intent.putExtra(EXTRA_MESSAGE, nameField.getText().toString());
+        startActivity(intent);
+    }
+
+    public void goToUpload(View view) {
+        Intent intent = new Intent(this, Upload.class);
         startActivity(intent);
     }
 
